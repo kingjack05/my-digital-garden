2bfc0373a282f815fdb2f289160bc7747df617c5
kingjack05 <kingjack05@gmail.com>
2022-06-03 08:23:20 +0800

    init NetlifyCMS

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,12 @@
-import { h } from 'preact';
-import Styles from './styles.module.scss';
+import { h } from "preact"
+import Styles from "./styles.module.scss"
 
 function Footer() {
 	return (
 		<footer className={Styles.footer}>
-			&copy; {new Date().getFullYear()} Jeanine White
+			&copy; {new Date().getFullYear()} Jack Tsai
 			<small className={Styles.byline}>🚀 Built by Astro</small>
 		</footer>
-	);
+	)
 }
-export default Footer;
+export default Footer
